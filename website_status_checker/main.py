4aeabb33a676245e01e3919747a80510fbea26fb
eerohin <eerohin@gear-games.com>
2025-04-19 17:02:55 +0300

    Add: docstrings for script

--- website_status_checker/main.py
+++ website_status_checker/main.py
@@ -16,14 +16,28 @@ TELEGRAM_API_URL = "https://api.telegram.org/bot"
 
 
 def check_telegram_credentials() -> bool:
+    """
+    Checks if all required environment variables for Telegram are set.
+
+    Returns:
+        bool: True if all required environment variables are set, False otherwise.
+    """
     return all([TELEGRAM_API_URL, TELEGRAM_API_TOKEN, TELEGRAM_CHAT_ID])
 
 
 def load_config(config_path: str) -> dict:
     """
-    Load config file
-    :param config_path:
-    :return:
+    Loads configuration from a JSON file.
+
+    Args:
+        config_path (str): The path to the JSON file containing the configuration.
+
+    Returns:
+        dict: The loaded configuration as a dictionary.
+
+    Raises:
+        FileNotFoundError: If the config file is not found.
+        json.JSONDecodeError: If the config file is not valid.
     """
     try:
         with open(config_path, 'r') as f:
@@ -38,6 +52,18 @@ def load_config(config_path: str) -> dict:
 
 
 def send_to_telegram(message: str) -> bool:
+    """
+    Sends a message to Telegram chat.
+
+    Args:
+        message (str): The message to send.
+
+    Returns:
+        bool: True if the message was sent successfully, False otherwise.
+
+    Raises:
+        requests.exceptions.RequestException: If the request fails.
+    """
     try:
         url = f"{TELEGRAM_API_URL}{TELEGRAM_API_TOKEN}/sendMessage"
         params = {"chat_id": TELEGRAM_CHAT_ID, "text": message}
@@ -54,12 +80,34 @@ def send_to_telegram(message: str) -> bool:
 
 
 def check_status(status: int) -> bool:
+    """
+    Checks if the status code is 200 OK.
+
+    Args:
+        status (int): The status code to check.
+
+    Returns:
+        bool: True if the status code is 200 OK, False otherwise.
+    """
     if status == http.HTTPStatus.OK:
         return True
     return False
 
 
 def request_to_host(url: str) -> int | None:
+    """
+    Sends a GET request to the specified URL and returns the status code.
+
+    Args:
+        url (str): The URL to send the request to.
+
+    Returns:
+        int | None: The HTTP status code if the request was successful, otherwise None.
+
+
+    Raises:
+        requests.exceptions.RequestException: If the request fails.
+    """
     try:
         response = requests.get(url, timeout=5)
         logging.info(f"Request to url: {url}")
@@ -69,6 +117,14 @@ def request_to_host(url: str) -> int | None:
 
 
 def notify_status_change(url: str, is_ok: bool, site_status: Dict[str, bool]) -> None:
+    """
+    Notifies the user via Telegram if the status of a website has changed.
+
+    Args:
+        url (str): The URL of the website.
+        is_ok (bool): True if the website is up, False otherwise.
+        site_status (Dict[str, bool]): A dictionary that stores the status of each website.
+    """
     if not is_ok and site_status[url]:
         text = f"Resource is down! URL: {url}"
         if send_to_telegram(message=text):
@@ -79,7 +135,19 @@ def notify_status_change(url: str, is_ok: bool, site_status: Dict[str, bool]) ->
             site_status[url] = is_ok
 
 
-def main():
+def main() -> None:
+    """
+    The main function of the script.
+
+    1. Checks if all required environment variables are set
+    2. Loads configuration from a JSON file
+    3. Set interval to check websites every 5 minutes
+    4. Sends a GET request to each URL and checks the status code
+    5. Sends notifications to Telegram chat if the status of a website has changed(Down to Up).
+
+    Raises:
+        Exception if happens any error via execution
+    """
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s - %(levelname)s - %(message)s",
